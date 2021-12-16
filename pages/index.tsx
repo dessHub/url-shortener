eeec7a8450d7f3976d4aaceb5f8052bfcd3f980b
desshub <desshub95@gmail.com>
2021-12-16 13:39:25 +0300

    Design URLs card

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from 'next'
 import { useState } from 'react';
 import { Urls, Url } from '@/types';
 import Styled from '@/components/pages/HomePage.styled';
+import StyledListCard from '@/components/ListCard.styled';
 import Header from '@/components/Header.component';
 import InputComponent from '@/components/InputComponent.component';
+import ListCard from '@/components/ListCard.component';
 import { Colors } from '@/ui';
 
 const Home: NextPage = () => {
@@ -24,6 +26,14 @@ const Home: NextPage = () => {
   }
 
   const handleSave = () => {
+    console.log('save')
+  }
+
+  const handleCopy = () => {
+    console.log('copy')
+  }
+
+  const handleDelete = () => {
     console.log('delete')
   }
 
@@ -42,8 +52,22 @@ const Home: NextPage = () => {
             required
             handleSave={handleSave}
             isError={isError}
-          />
+        />
+        {isError && <StyledListCard.Text mt={20} fs={12} color={Colors.warning}>Add a URL to shorten.</StyledListCard.Text>}
       </Styled.Card>
+
+      {urls.length > 0 && urls.map(item => {
+      return (
+        <Styled.Card mt={10} key={item.id}>
+          <ListCard url={item} handleCopy={handleCopy} handleDelete={handleDelete} />
+        </Styled.Card>
+      )})}
+
+      {urls.length === 0 && (
+        <Styled.Card mt={40} p='40px'>
+          <StyledListCard.Text>You have 0 Shortened URLs. Shorten some URLs</StyledListCard.Text>
+        </Styled.Card>
+      )}
     </Styled.Container>
   )
 }
